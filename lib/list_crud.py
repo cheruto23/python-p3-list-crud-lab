04ed39a7197dfdc498c7c13e68cea80d7ee5a1ac
Joy <joyglory213@gmail.com>
2023-11-27 12:48:11 +0300

    Completed

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,30 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return [1,2,'Joy','run']
 
-def add_element_to_end_of_list(l, element):
-    return None
+def add_element_to_end_of_list(my_list, element):
+    my_list.append(element)
+    return my_list
 
-def add_element_to_start_of_list(l, element):
-    return None
+def add_element_to_start_of_list(new_list, element):
+    new_list.insert(0,element)
+    return new_list
 
-def remove_element_from_end_of_list(l):
-    return None
+def remove_element_from_end_of_list(list_remove):
+    list_remove.pop()
+    return list_remove
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_start_of_list(list_del): 
+    del list_del[0]
+    return list_del
 
-def retrieve_first_element_from_list(l):
-    return None
+def retrieve_first_element_from_list(list_return):
+    return list_return[0]
 
-def retrieve_element_from_index(l, index):
-    return None
+def retrieve_element_from_index(list_retrieve, index):
+    return list_retrieve[1]
 
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_last_element_from_list(retrieve_last_element):
+    return retrieve_last_element[-1]
